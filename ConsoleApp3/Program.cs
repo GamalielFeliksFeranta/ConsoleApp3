adc67b0e35bbe424f9677c6adfd16b6ac7542b67
Gamaliel Feliks <gamalielferanta@gmail.com>
2021-12-03 10:38:37 +0700

    Membuat program mengechek bilangan prima

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -8,9 +8,31 @@ namespace ConsoleApp3
 {
     internal class Program
     {
+        private const int a = 1;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.Write("Masukan Input : ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            int temp = 0;
+            for (int i = 1; i < input + a;i++ )
+            {
+                temp = input % i;
+                if (temp == 0)
+                {
+                   count++;
+                }
+            }
+            if (count > 2)
+            {
+                Console.Write("Bukan prima");
+
+            }
+            else
+            {
+                Console.Write("Bilangan prima");
+            }
+            
         }
     }
 }
